def02030cc911586583678986366bd4c2a427c38
zitriss <zitrissonline@gmail.com>
2025-10-18 18:05:08 +0200

    oops

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:38:28 by tlize             #+#    #+#             */
-/*   Updated: 2025/10/18 17:27:06 by tlize            ###   ########.fr       */
+/*   Updated: 2025/10/18 18:04:33 by tlize            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ class Zombie
 public:
     Zombie(std::string name);
     ~Zombie(void);
-    
-    void        announce(void);
+    void    announce(void);
 private:
     std::string name;
 };
