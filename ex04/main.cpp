1dd08f369b463bbea50cd22c8306e52f07f26c31
zitriss <zitrissonline@gmail.com>
2025-10-22 18:37:41 +0200

    checks if file exists

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,24 +6,35 @@
 /*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:56:41 by tlize             #+#    #+#             */
-/*   Updated: 2025/10/22 18:10:24 by tlize            ###   ########.fr       */
+/*   Updated: 2025/10/22 18:34:05 by tlize            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-int	checkFilename(std::string& filename){
-    
+bool	checkFilename(char *filename)
+{
+	std::string filenamestr = filename;
+    std::ifstream file(filename);
+	return (file.good());
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+	char *filename;
+	
     if (argc != 4)
     {
-        std::cout << "wrong number of arguments" << std::endl;
+        std::cout << "Wrong number of arguments" << std::endl;
         return (0);        
     }
-	std::string filename = argv[1];
+	filename = argv[1];
+	if (!checkFilename(filename))
+	{
+	    std::cout << "File does not exist" << std::endl;
+		return (0);	
+	}
     std::ofstream test;
     test.open (filename);
     test.close();
